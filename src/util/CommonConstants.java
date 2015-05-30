4693f3883182a2bc7e8c93fb744fdb6e1f705679
Uy Nguyễn <leesan.itus@gmail.com>
2015-05-30 10:20:00 +0700

    First commit

---
+++ src/util/CommonConstants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author UyNguyen.ITUS
+ */
+public class CommonConstants {
+    
+    public static final String _classNameDriver = "org.postgresql.Driver";
+    
+    //"jdbc:postgresql://localhost:[port]/[tênCSDL]"
+    
+    public static final String _urlConnection = "jdbc:postgresql://localhost:5432/mygoogledocs"; // Đổi tên database của bạn 
+   
+    public static final String _passwordConnection =  "uy10119114";  // Đổi bằng mật khẩu CSDL của bạn
+    
+    public static final String _userConnection = "postgres";
+    
+}
