4c5740880edeac76bc22c39e333648e3eac9a1af
Uy Nguyễn <leesan.itus@gmail.com>
2015-05-30 18:07:41 +0700

    Script db

---
+++ ScriptDB/DOCUMENTS.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE documents
+(
+  "ID" serial NOT NULL,
+  "Name" text,
+  "Path" text,
+  "DateCreate" date,
+  "IDOwner" integer,
+  "IDPartners" integer,
+  CONSTRAINT documents_pkey PRIMARY KEY ("ID"),
+        CONSTRAINT "documents_IDOwner_fkey" FOREIGN KEY ("IDOwner")
+      REFERENCES accounts ("ID")
+)
---
+++ ScriptDB/LISTFRIENDS.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE listfriends
+(
+  "ID" serial NOT NULL,
+  "User" text,
+  "Friend" text,
+  CONSTRAINT listfriends_pkey PRIMARY KEY ("ID"),
+    CONSTRAINT "lisfriends_User_fkey" FOREIGN KEY ("User")
+      REFERENCES accounts ("UserName"),
+      CONSTRAINT "lisfriends_Friend_fkey" FOREIGN KEY ("Friend")
+      REFERENCES accounts ("UserName")
+
+  
+)
