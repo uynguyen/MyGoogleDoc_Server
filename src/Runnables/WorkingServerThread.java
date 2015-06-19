33949f16c9b1d6b58d8091ed6d7da1bfeefb9f2a
Tran Trong Thanh Tung <tttt.conan@gmail.com>
2015-06-19 16:33:26 +0700

    thêm các lớp bên server để làm

--- src/Runnables/WorkingServerThread.java
+++ src/Runnables/WorkingServerThread.java
@@ -6,6 +6,8 @@
 package Runnables;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -18,11 +20,13 @@ import java.util.logging.Logger;
 public class WorkingServerThread implements Runnable{
     
     Thread t;
-    ServerSocket server;    
+    ServerSocket server;
+    Notifier notifier;
     
     public WorkingServerThread(ServerSocket server){
         this.server = server;        
         t = new Thread(this);
+        notifier = new Notifier();
         t.start();
     }
 
@@ -30,9 +34,23 @@ public class WorkingServerThread implements Runnable{
     public void run() {
         do{
             try {
+                //accept a port
                 Socket client = server.accept();
                 
+                System.out.println(client.getPort());
                 
+                //get outputstream
+                ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+                oos.flush();
+                
+                //Register client outputStream
+                notifier.Register(oos);
+                
+                //get inputStream
+                ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+                
+                //create another thread to receive
+                ClientReceiveThread clientReceiveThread = new ClientReceiveThread(ois, notifier);
                 
             } catch (IOException ex) {
                 Logger.getLogger(WorkingServerThread.class.getName()).log(Level.SEVERE, null, ex);
